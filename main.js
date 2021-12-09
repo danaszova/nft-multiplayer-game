caae99e33ff414acba4166c905c08579d503500b
Dana Szova <dana.s.szova@gmail.com>
2021-12-09 13:00:11 -0800

    initialize moralis and add phaser library

---
+++ main.js
@@ -0,0 +1,64 @@
+/** Connect to Moralis server */
+const serverUrl = "https://5iod3lrqrtc2.usemoralis.com:2053/server";
+const appId = "ksAzHZscHJAMrd9qFFWyoQ5pwShCnRW32CEjplFe";
+Moralis.start({ serverUrl, appId });
+
+/** Add from here down */
+async function login() {
+  let user = Moralis.User.current();
+  if (!user) {
+   try {
+      user = await Moralis.authenticate({ signingMessage: "Hello World!" })
+      console.log(user)
+      console.log(user.get('ethAddress'))
+   } catch(error) {
+     console.log(error)
+   }
+  }
+}
+
+async function logOut() {
+  await Moralis.User.logOut();
+  console.log("logged out");
+}
+
+document.getElementById("btn-login").onclick = login;
+document.getElementById("btn-logout").onclick = logOut;
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+}
+
+function create ()
+{
+}
+
+function update ()
+{
+}
+
+
+
+
+/** Useful Resources  */
+
+// https://docs.moralis.io/moralis-server/users/crypto-login
+// https://docs.moralis.io/moralis-server/getting-started/quick-start#user
+// https://docs.moralis.io/moralis-server/users/crypto-login#metamask
+
+/** Moralis Forum */
+
+// https://forum.moralis.io/
